cc863f336edfd956c96b6cb78cb26ebcb19c43db
ttavarone <th26tava@siena.edu>
2018-04-11 17:24:44 -0400

    Updated method "getFiles" so that it can get files and then place them in the resources directory

--- src/InputReader.java
+++ src/InputReader.java
@@ -1,6 +1,7 @@
 import sun.dc.path.PathException;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
@@ -8,43 +9,82 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class InputReader {
+    private static final Throwable FileNotSelectedException = null;
     private Scanner sc1;
     private Scanner sc2;
     private File firstDocIn;
     private File secondDocIn;
+    private String firstDocName;
+    private String secondDocName;
 
     public InputReader() {
         sc1 = null;
         sc2 = null;
         firstDocIn = null;
         secondDocIn = null;
+        firstDocName = new String();
+        secondDocName = new String();
     }
 
+    /**
+     * Gets files and places them in the resources folder
+     */
     public void getFiles() {
         try {
-            final JFileChooser fc = new JFileChooser();
+            JFileChooser fc = new JFileChooser();
+
             int returnVal = fc.showOpenDialog(null);
-
             firstDocIn = fc.getSelectedFile();
-            secondDocIn = fc.getSelectedFile();
+            if(returnVal == JFileChooser.CANCEL_OPTION ||
+            returnVal == JFileChooser.ERROR_OPTION) {
+                throw new FileNotSelectedException();
+            }
 
-            String firstDocName = firstDocIn.toString();
-            String secondDocName = secondDocIn.toString();
+            returnVal = fc.showOpenDialog(null);
+            secondDocIn = fc.getSelectedFile();
+            if(returnVal == JFileChooser.CANCEL_OPTION ||
+                    returnVal == JFileChooser.ERROR_OPTION) {
+                throw new FileNotSelectedException();
+            }
+
+            firstDocName = firstDocIn.toString();
+            secondDocName = secondDocIn.toString();
 
             Path resourcesDir = Paths.get("/TextCompare/resources/");
 
-        }catch(FileNotFoundException e) {
+            String path = resourcesDir.toString();
+            path.concat(File.separator+firstDocName);
+            firstDocIn = new File(path);
+
+            String path2 = resourcesDir.toString();
+            path2.concat(File.separator+secondDocName);
+            secondDocIn = new File(path2);
+
+
+        } catch (FileNotSelectedException fs) {
+            System.err.print("No file was selected.")
+
+        } catch (FileNotFoundException e) {
             System.err.print("Error finding file!\n");
             System.err.print("Please restart the program to try again.");
-        }
-        catch(PathException p) {
+        } catch (PathException p) {
             System.err.print("Path to resources folder is invalid.");
             //unlikely this error will be thrown until the program
-            //is completely finished and tested on a new machine
+            //is completely finished and tested on a new device
+        } catch (HeadlessException h) {
+            System.err.print("Headless Exception... Look it up.")
         }
     }
+
+}
 
     public boolean isEqual() {
 
     }
+
+    class FileNotSelectedException extends Exception {
+    public FileNotSelectedException() {
+        super();
+    }
+}
 }
