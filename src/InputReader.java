95f82b5d87a4a6191eb6e0efb81756fc57ccfd7c
ttavarone <th26tava@siena.edu>
2018-04-11 16:36:53 -0400

    Added try/catch blocks and lines that can take the code in and create a file

--- src/InputReader.java
+++ src/InputReader.java
@@ -1,6 +1,10 @@
+import sun.dc.path.PathException;
+
 import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class InputReader {
@@ -17,12 +21,27 @@ public class InputReader {
     }
 
     public void getFiles() {
-        //Create a file chooser
-        final JFileChooser fc = new JFileChooser();
-        int returnVal = fc.showOpenDialog(null);
+        try {
+            final JFileChooser fc = new JFileChooser();
+            int returnVal = fc.showOpenDialog(null);
 
-        firstDocIn = fc.getSelectedFile();
-        secondDocIn = fc.getSelectedFile();
+            firstDocIn = fc.getSelectedFile();
+            secondDocIn = fc.getSelectedFile();
+
+            String firstDocName = firstDocIn.toString();
+            String secondDocName = secondDocIn.toString();
+
+            Path resourcesDir = Paths.get("/TextCompare/resources/");
+
+        }catch(FileNotFoundException e) {
+            System.err.print("Error finding file!\n");
+            System.err.print("Please restart the program to try again.");
+        }
+        catch(PathException p) {
+            System.err.print("Path to resources folder is invalid.");
+            //unlikely this error will be thrown until the program
+            //is completely finished and tested on a new machine
+        }
     }
 
     public boolean isEqual() {
